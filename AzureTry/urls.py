549d59454ab2e649a56e759740867e861d953b75
Naman Varma <namanvrm7@gmail.com>
2020-04-01 02:42:54 +0530

    still problem with serving static files

--- AzureTry/urls.py
+++ AzureTry/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('',include('mysite.urls')),
     path('Food/',include('Food.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
